cbaeada3fb230435ae6df83dc4aafc6e56ce335b
Youssef abdelouali <youssefabdelouali0@gmail.com>
2023-06-19 12:22:47 -0700

    Task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point of the program
  *
- * Description: This program prints all single-digit numbers of base 10 starting from 0,
+ * Description: Prints all single-digit numbers of base 10 starting from 0,
  *              followed by a new line, using putchar.
  *
  * Return: Always 0 (Success)
