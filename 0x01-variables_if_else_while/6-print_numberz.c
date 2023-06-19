5f9e7b47876e7c2e41c2937227cdc9ed4fc55bfa
Youssef abdelouali <youssefabdelouali0@gmail.com>
2023-06-19 12:06:50 -0700

    Task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,18 +3,18 @@
 /**
  * Prints all single-digit numbers of base 10 starting from 0,
  * followed by a new line, using putchar.
- * Returns: Always 0 (Success).
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int digit = 48;
+	int digit = 48;
 
-    while (digit < 58)
-    {
-        putchar(digit);
-        digit++;
-    }
+	while (digit < 58)
+	{
+		putchar(digit);
+		digit++;
+	}
 
-    putchar('\n');
-    return 0;
+	putchar('\n');
+	return 0;
 }
