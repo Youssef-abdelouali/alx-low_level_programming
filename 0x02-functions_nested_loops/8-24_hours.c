d08bbee091a88a4de7e7a189f0d04a0365009120
Youssef abdelouali <youssefabdelouali0@gmail.com>
2023-06-21 18:20:50 -0700

    Task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of the day of Jack Bauer
+ *              starting from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	hour = 0;
+
+	do {
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+		}
+		hour++;
+	} while (hour < 24);
+}
