9908ee1aff42e41f31e5f997f7120cfdf0ce71c1
Youssef abdelouali <youssefabdelouali0@gmail.com>
2023-06-21 18:54:22 -0700

    Task 10 AND 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - Prints all natural numbers from n to 98, followed by a new line
+ * @num: The starting number
+ */
+void print_to_98(int num)
+{
+	int current;
+
+	if (num <= 98)
+	{
+		/* Loop from num to 98 */
+		current = num;
+		while (current <= 98)
+		{
+			printf("%d", current);
+			if (current != 98)
+				printf(", ");
+			else
+				printf("\n");
+			current++;
+		}
+	}
+	else
+	{
+		/* Loop from num to 98 */
+		current = num;
+		while (current >= 98)
+		{
+			printf("%d", current);
+			if (current != 98)
+				printf(", ");
+			else
+				printf("\n");
+			current--;
+		}
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -320,3 +320,31 @@ void times_table(void)
 
 
 ```
+10. a + b
+
+**Write a function that adds two integers and returns the result :**
+
+```
+#include "main.h"
+
+/**
+ * add - Adds two integers and returns the result
+ * @num1: First integer to be added
+ * @num2: Second integer to be added
+ *
+ * Return: Sum of num1 and num2
+ */
+int add(int num1, int num2)
+{
+	int sum;
+
+	/* Calculate the sum of num1 and num2 */
+	sum = num1 + num2;
+
+	/* Return the result */
+	return sum;
+}
+
+
+
+```
