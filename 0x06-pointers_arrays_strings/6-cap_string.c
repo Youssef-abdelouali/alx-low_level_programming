80643655dd1864a31799ad9b5ea024533e2792e0
Youssef abdelouali <youssefabdelouali0@gmail.com>
2023-06-30 06:59:34 -0700

    Tasks

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,35 +1,33 @@
 #include "main.h"
 
 /**
- * cap_string - Capitalizes the first letter of each word in a string.
+ * cap_string - Capitalizes all words of a string.
  * @str: The string to be capitalized.
  *
- * Return: A pointer to the modified string.
+ * Return: A pointer to the changed string.
  */
 char *cap_string(char *str)
 {
 	int index = 0;
 
-	/* Capitalize the first letter of each word */
-	do {
-		/* Find the next lowercase letter */
+	while (str[index])
+	{
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
 
-		/* Capitalize the letter if it's the first letter of a word */
 		if (str[index - 1] == ' ' || str[index - 1] == '\t' ||
 		    str[index - 1] == '\n' || str[index - 1] == ',' ||
 		    str[index - 1] == ';' || str[index - 1] == '.' ||
 		    str[index - 1] == '!' || str[index - 1] == '?' ||
 		    str[index - 1] == '"' || str[index - 1] == '(' ||
 		    str[index - 1] == ')' || str[index - 1] == '{' ||
-		    str[index - 1] == '}')
+		    str[index - 1] == '}' || index == 0)
 		{
-			str[index] -= 32; /* Convert lowercase to uppercase */
+			str[index] -= 32;
 		}
 
 		index++;
-	} while (str[index]);
+	}
 
 	return (str);
 }
