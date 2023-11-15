787ebfb2d544c34c4f045d26a15cacc97fd1f34d
Youssef-abdelouali <youssefabdelouali225@gmail.com>
2023-11-15 05:44:44 -0800

    i fixed the error

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,40 +10,41 @@
  */
 int **alloc_grid(int width, int height)
 {
-    int **grid;
-    int x, y;
+	int **grid;
+	int x, y;
 
-    if (width <= 0 || height <= 0)
-        return (NULL);
+	if (width <= 0 || height <= 0)
+		return (NULL);
 
-    /* Allocate memory for the rows */
-    grid = malloc(sizeof(int *) * height);
+	/* Allocate memory for the rows */
+	grid = malloc(sizeof(int *) * height);
 
-    if (grid == NULL)
-        return (NULL);
+	if (grid == NULL)
+		return (NULL);
 
-    /* Allocate memory for each row */
-    for (x = 0; x < height; x++)
-    {
-        grid[x] = malloc(sizeof(int) * width);
+	/* Allocate memory for each row */
+	for (x = 0; x < height; x++)
+	{
+		grid[x] = malloc(sizeof(int) * width);
 
-        /* Check for allocation failure and free previously allocated memory */
-        if (grid[x] == NULL)
-        {
-            for (; x >= 0; x--)
-                free(grid[x]);
+		/* Check for allocation failure and free previously allocated memory */
+		if (grid[x] == NULL)
+		{
+			for (; x >= 0; x--)
+				free(grid[x]);
 
-            free(grid);
-            return (NULL);
-        }
-    }
+			free(grid);
+			return (NULL);
+		}
+	}
 
-    /* Initialize the elements of the grid to 0 */
-    for (x = 0; x < height; x++)
-    {
-        for (y = 0; y < width; y++)
-            grid[x][y] = 0;
-    }
+	/* Initialize the elements of the grid to 0 */
+	for (x = 0; x < height; x++)
+	{
+		for (y = 0; y < width; y++)
+			grid[x][y] = 0;
+	}
 
-    return (grid);
+	return (grid);
 }
+
