a04753262d65910aff2d79b290d94ee4c882b940
Youssef-abdelouali <youssefabdelouali225@gmail.com>
2024-03-23 09:20:18 -0700

    tasks resolved

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -5,24 +5,75 @@
 /**
  * find_largest_char - finds the biggest number
  *
- * @username: username
+ * @usrn: username
+ * @len: length of username
  * Return: the biggest number
  */
-int find_largest_char(char *username)
+int find_largest_char(char *usrn, int len)
 {
-    int ch;
-    int vch;
+    int character;
+    int index;
+    unsigned int rand_num;
 
-    ch = vch = 0;
+    character = *usrn;
+    index = 0;
 
-    while (vch < *username)
+    while (index < len)
     {
-        unsigned int random_number = rand();
-        ch = random_number;
-        vch += 1;
+        if (character < usrn[index])
+            character = usrn[index];
+        index += 1;
     }
 
-    return (((unsigned int)ch ^ 229) & 63);
+    srand(character ^ 14);
+    rand_num = rand();
+
+    return (rand_num & 63);
+}
+
+/**
+ * multiply_chars - multiplies each char of username
+ *
+ * @usrn: username
+ * @len: length of username
+ * Return: multiplied char
+ */
+int multiply_chars(char *usrn, int len)
+{
+    int character;
+    int index;
+
+    character = index = 0;
+
+    while (index < len)
+    {
+        character = character + usrn[index] * usrn[index];
+        index += 1;
+    }
+
+    return (((unsigned int)character ^ 239) & 63);
+}
+
+/**
+ * generate_random_char - generates a random char
+ *
+ * @usrn: username
+ * Return: a random char
+ */
+int generate_random_char(char *usrn)
+{
+    int character;
+    int index;
+
+    character = index = 0;
+
+    while (index < *usrn)
+    {
+        character = rand();
+        index += 1;
+    }
+
+    return (((unsigned int)character ^ 229) & 63);
 }
 
 /**
@@ -35,7 +86,7 @@ int find_largest_char(char *username)
 int main(int argc, char **argv)
 {
     char keygen[7];
-    int len, ch, vch;
+    int len, character, index;
     long alph[] = {
         0x3877445248432d41, 0x42394530534e6c37, 0x4d6e706762695432,
         0x74767a5835737956, 0x2b554c59634a474f, 0x71786636576a6d34,
@@ -47,30 +98,30 @@ int main(int argc, char **argv)
     /* ----------- f1 ----------- */
     keygen[0] = ((char *)alph)[(len ^ 59) & 63];
     /* ----------- f2 ----------- */
-    ch = vch = 0;
-    while (vch < len)
+    character = index = 0;
+    while (index < len)
     {
-        ch = ch + argv[1][vch];
-        vch = vch + 1;
+        character = character + argv[1][index];
+        index = index + 1;
     }
-    keygen[1] = ((char *)alph)[(ch ^ 79) & 63];
+    keygen[1] = ((char *)alph)[(character ^ 79) & 63];
     /* ----------- f3 ----------- */
-    ch = 1;
-    vch = 0;
-    while (vch < len)
+    character = 1;
+    index = 0;
+    while (index < len)
     {
-        ch = argv[1][vch] * ch;
-        vch = vch + 1;
+        character = argv[1][index] * character;
+        index = index + 1;
     }
-    keygen[2] = ((char *)alph)[(ch ^ 85) & 63];
+    keygen[2] = ((char *)alph)[(character ^ 85) & 63];
     /* ----------- f4 ----------- */
-    keygen[3] = ((char *)alph)[find_largest_char(argv[1])];
+    keygen[3] = ((char *)alph)[find_largest_char(argv[1], len)];
     /* ----------- f5 ----------- */
-    keygen[4] = ((char *)alph)[find_largest_char(argv[1])];
+    keygen[4] = ((char *)alph)[multiply_chars(argv[1], len)];
     /* ----------- f6 ----------- */
-    keygen[5] = ((char *)alph)[find_largest_char(argv[1])];
+    keygen[5] = ((char *)alph)[generate_random_char(argv[1])];
     keygen[6] = '\0';
-    for (ch = 0; keygen[ch]; ch++)
-        printf("%c", keygen[ch]);
+    for (index = 0; keygen[index]; index++)
+        printf("%c", keygen[index]);
     return (0);
 }
